6805b855aee400c21448343d0a2772ae39bcf81b
LarissaPadilha-dev <larissapadilha436@gmail.com>
2024-11-18 21:07:54 -0300

    :sparkles: feat: Configurado as Rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Header from "./Components/Header/header";
+import './Components/Reset/global.scss'
 
 export default function App() {
   return (
--- src/Components/Header/header.jsx
+++ src/Components/Header/header.jsx
@@ -1,22 +1,38 @@
 import logo from "../../assets/Header/logo.png";
+import busca from "../../assets/Header/lupa.png";
+import {BrowserRouter,Routes,Route,Link} from 'react-router-dom';
+import Inicio from "../../Pages/Inicio/Inicio";
+import Doados from "../../Pages/Doados/Doados";
+import QueroDoar from "../../Pages/QueroDoar/QueroDoar";
+import S from "./header.module.scss"
+
 
 export default function header() {
     return (
+      <BrowserRouter>
       <header>
-          <section>
+          <section className={S.boxLogo}>
               <img src={logo} alt="imagem de logo de livros" />
               <h1>Livros Vai na web</h1>
           </section>
           <ul>
-            <li>Início</li>
-            <li>Livros Doados</li>
-            <li>Quero Doar</li>
+            <li><Link to="/">Início</Link></li>
+            <li><Link to="/doados">Livros Doados</Link></li>
+            <li><Link to="/queroDoar">Quero Doar</Link></li>
           </ul>
+          <div>
           <input
-            name="search"
             type="text"
             placeholder="O que você procura?"
           />
+          <img src={busca} alt="icone de busca" />
+          </div>
       </header>
+      <Routes>
+        <Route path='/' element={<Inicio/>} />
+        <Route path='/doados' element={<Doados/>} />
+        <Route path='/queroDoar' element={<QueroDoar/>} />
+      </Routes>
+      </BrowserRouter>
     )
   }
