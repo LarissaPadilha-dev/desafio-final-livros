3c44a24696901ac675c90671ac1d452180710abb
LarissaPadilha-dev <larissapadilha436@gmail.com>
2024-12-04 10:36:42 -0300

    Header finalizada e responsiva:tada:

--- src/Components/Header/header.jsx
+++ src/Components/Header/header.jsx
@@ -1,22 +1,29 @@
 import logo from "../../assets/Header/logo.png";
 import busca from "../../assets/Header/lupa.png";
 import {Link} from 'react-router-dom';
+import { useState } from "react";
 import S from "./header.module.scss"
 
 
 export default function Header() {
+  const [menuAberto, setMenuAberto] = useState(false);
+
+  const toggleMenu = () => setMenuAberto(!menuAberto);
     return (
       <header className={S.header}>
           <section className={S.boxLogo}>
               <img src={logo} alt="imagem de logo de livros" />
               <h1>Livros Vai na web</h1>
           </section>
-          <nav className={S.paginas}>
-          <ul>
-            <li><Link className={S.link} to="/">Início</Link></li>
-            <li><Link className={S.link}to="/doados">Livros Doados</Link></li>
-            <li><Link className={S.link}to="/queroDoar">Quero Doar</Link></li>
-          </ul>
+          <button className={S.hamburger} onClick={toggleMenu}>
+          ☰
+          </button>
+          <nav className={`${S.paginas} ${menuAberto ? S.ativo : ""}`}>
+            <ul>
+              <li><Link className={S.link} to="/">Início</Link></li>
+              <li><Link className={S.link} to="/doados">Livros Doados</Link></li>
+              <li><Link className={S.link} to="/queroDoar">Quero Doar</Link></li>
+            </ul>
           </nav>
           <div className={S.busca}>
                 <input type="text" placeholder='O que você procura?' />
