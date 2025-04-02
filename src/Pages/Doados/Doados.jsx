b7b090e9ce51ed497d5184ea865127287c0161bf
LarissaPadilha-dev <larissapadilha436@gmail.com>
2025-04-02 17:28:31 -0300

    integração do front-end com o back-end finalizada

--- src/Pages/Doados/Doados.jsx
+++ src/Pages/Doados/Doados.jsx
@@ -1,7 +1,25 @@
 import S from "./doados.module.scss"
 import livro from "../../assets/Doados/livro.png"
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 
 export default function Doados() {
+
+  const [livros, setLivros] = useState([])
+
+  const getLivros = async () => {
+    try {
+        const response = await axios.get("https://desafio-2-api-livros-vai-na-web-1al1.onrender.com/livros")
+        setLivros(response.data)
+    } catch (error) {
+        console.error("Erro ao buscar livros:", error)
+    }
+}
+
+  useEffect(() => {
+  getLivros()
+}, [])
+
   return (
     <main className={S.doados}>
         <h2>Livros Doados</h2>
@@ -22,8 +40,16 @@ export default function Doados() {
               <img src={livro} alt= "imagem relacionada aos livros doados" />
               <h3>O protagonista</h3>
               <p>Susanne Andrade</p>
-               <p>Ficção</p>
+              <p>Ficção</p>
           </article>
+          {livros.map((item) => (
+          <article key={item.id}>
+              <img src={item.imagem_url} alt={item.titulo} />
+              <h3>{item.titulo}</h3>
+              <p>{item.autor}</p>
+              <p>{item.categoria}</p>
+          </article>
+          ))}
         </section>
     </main>
   )
--- src/Pages/QueroDoar/QueroDoar.jsx
+++ src/Pages/QueroDoar/QueroDoar.jsx
@@ -10,18 +10,19 @@ export default function QueroDoar(){
     const[autor,setAutor] = useState("")
     const[imagem_url,setImagem_url] = useState("")
 
-    const enviarDados = async()=>{
-        const urlApi = "https://desafio-2-api-livros-vai-na-web-1al1.onrender.com" 
-
-        const dadosEnviar = {
-            titulo,
-            categoria,
-            autor,
-            imagem_url
-        }
-        await axios.post(urlApi, dadosEnviar)
-
-        alert("Livro cadastrado com sucesso!!")
+    const enviarDados = async() =>{
+      const urlApi = "https://desafio-2-api-livros-vai-na-web-1al1.onrender.com/doar"; 
+      
+      const dadosEnviar = {
+        titulo,
+        categoria,
+        autor,
+        imagem_url
+      }
+        
+      await axios.post(urlApi, dadosEnviar)
+        
+      alert("Livro cadastrado com sucesso!!")
 
         setTitulo("")
         setCategoria("")
@@ -29,6 +30,7 @@ export default function QueroDoar(){
         setImagem_url("")
     }
 
+  
     const capturaTitulo = (e) =>{
         setTitulo(e.target.value)
         console.log(e.target.value)
@@ -60,7 +62,7 @@ export default function QueroDoar(){
                     <input type="text" placeholder='Categoria' onChange={capturaCategoria} value={categoria} />
                     <input type="text" placeholder='Autor' onChange={capturaAutor} value={autor}  />
                     <input type="text" placeholder='Link da Imagem' onChange={capturaImagem} value={imagem_url} />
-                    <input type="submit" value="Doar" onClick={enviarDados} />
+                    <input onClick={enviarDados} type="submit" value="Doar" />
                 </form>
             </section>
         </section>
